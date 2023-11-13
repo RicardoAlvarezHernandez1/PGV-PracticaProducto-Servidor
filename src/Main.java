13d0ba6ce7127c83b583ac5be66379a267f79404
rica07b <rica07b@gmail.com>
2023-11-13 19:27:19 +0000

    feat : experimento 1 resuelto

--- src/Main.java
+++ src/Main.java
@@ -4,26 +4,16 @@ import net.salesianos.threads.Productor;
 
 public class Main {
     public static void main(String[] args) {
-        Almacen almacen = new Almacen(20);
+        Almacen almacen = new Almacen(5);
 
-        Productor Paquito = new Productor("Paquitoo", 10, almacen);
-        Productor Ramoncin = new Productor("Ramoncín", 10, almacen);
-        Consumidor Faustinito = new Consumidor("Faustino", 5 , 3000,almacen);
-        Consumidor MrGentlemansito = new Consumidor("Mr. Gentlemansito", 10, 6000,almacen);
+        Productor Paquito = new Productor("Paquitoo", 5, almacen);
         Consumidor Loquendito = new Consumidor("Loquendito", 5 ,2000 ,almacen);
     
         Paquito.start();
-        Ramoncin.start();
-
-        Faustinito.start();
-        MrGentlemansito.start();
         Loquendito.start();
 
         try {
             Paquito.join();
-            Ramoncin.join();
-            Faustinito.join();
-            MrGentlemansito.join();
             Loquendito.join();
         } catch (Exception e) {
             e.printStackTrace();
